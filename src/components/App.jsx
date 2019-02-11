60fb1f44cd7a2ecafc1ade4eb76e0c00fc20d749
Robert Bruce and Scott Bergler <scott@>
2019-02-11 14:36:34 -0800

    add Profile component & wireframe styles.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Profile from "./Profile";
 
 function App(){
   return (
     <div>
       <Header/>
+      <Profile/>
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Profile(){
+  const profileStyles = {
+    width: '30%',
+    height: '300px',
+    border: '1px solid blue',
+    marginTop: '5px'
+  }
+  return (
+    <div style={profileStyles}>
+      profile works
+    </div>
+  );
+}
+
+export default Profile;
