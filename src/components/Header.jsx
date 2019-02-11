594767a78500d9a9e09005f28c52ebbca488cff6
Robert Bruce and Scott Bergler <scott@>
2019-02-11 14:30:47 -0800

    style header wireframe.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Header(){
-  return ();
+  const headerStyles = {
+    width: '95%',
+    height: '50px',
+    border: '1px solid red'
+  }
+  return (
+    <div style={headerStyles}>
+      header works
+    </div>
+  );
 }
 
 export default Header;
